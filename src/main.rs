6a1feeb057a50525c24a6fe529f94081afe206db
Bing-Chang Lai <johnny.lai@me.com>
2025-06-19 09:33:18 -0700

    Responds to get and version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:11212").await?;
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let (read_half, write_half) = socket.into_split();
+            let mut reader = BufReader::new(read_half);
+            let mut writer = write_half;
+            let mut line = String::new();
+
+            loop {
+                line.clear();
+                let bytes = reader.read_line(&mut line).await.unwrap();
+                if bytes == 0 {
+                    break;
+                }
+
+                let v: Vec<&str> = line.split(char::is_whitespace).collect();
+                match v[0] {
+                    "get" => {
+                        println!("command get got {}", v[1]);
+                        let value = "response";
+                        writer
+                            .write_all(
+                                format!("VALUE {} 0 {}\r\n{}\r\nEND\r\n", v[1], value.len(), value)
+                                    .as_bytes(),
+                            )
+                            .await
+                            .unwrap();
+                    }
+                    "version" => {
+                        writer.write_all(b"VERSION 0.0.1\r\n").await.unwrap();
+                    }
+                    _ => {
+                        println!("unknown command {}", line);
+                        writer.write_all(b"ERROR\r\n").await.unwrap();
+                    }
+                }
+            }
+        });
+    }
+    Ok(())
 }
